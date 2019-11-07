01d716f2abcc6def95fd4d73a2f0c0f62748dc80
Robert Laws <roblaws@me.com>
2019-11-07 17:04:30 +0300

    add support for display of individual news items

--- src/components/News/news.component.jsx
+++ src/components/News/news.component.jsx
@@ -3,9 +3,8 @@ import { UserConsumer } from '../../App';
 
 const News = ({ myNews }) => {
   const [error, setError] = useState(false);
-  const [news, setNews] = useState({
-    news: []
-  });
+  const [data, setData] = useState([]);
+  const [info, setInfo] = useState(0);
 
   useEffect(() => {
     const url = `https://newsapi.org/v2/${myNews.type}?${myNews.query}&apiKey=ef895e4833db45119946a71b8c4530ba`;
@@ -15,22 +14,33 @@ const News = ({ myNews }) => {
         return response.json();
       })
       .then((data) => {
-        setNews({
-          news: data.articles
-        })
+        setData(data.articles)
       })
       .catch(error => {
         setError(true)
       })
   }, [myNews])
 
+  useEffect(() => {
+    setInfo(data)
+  }, [data])
+
+  const renderItems = news => {
+    if(news.length > 0) {
+      news.map(item => {
+        return item
+      })
+    }
+  }
+
   return (
     <UserConsumer>
       {({ firstName, lastName, data }) => (
       <div>
-        <p>{firstName}</p>
-        <p>{lastName}</p>
+        <p>{firstName} {lastName}</p>
         <p>{data}</p>
+        <hr />
+        <p>{info.length > 0 ? info.map(item => ( <p>{item.author}</p> )) : ''}</p>
       </div>
       )}
     </UserConsumer>    
