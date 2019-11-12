e165c9372e66f404cce151a674be59c2b922a641
Robert Laws <roblaws@me.com>
2019-11-12 17:18:11 +0300

    add useState for locations data

--- src/components/Locations/locations.component.jsx
+++ src/components/Locations/locations.component.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const Locations = () => {
+  const [error, setError] = useState('');
   const [city, setCity ] = useState('');
   const [country, setCountry] = useState('')
   const [locations, setLocations] = useState([])
@@ -8,13 +9,19 @@ const Locations = () => {
   const updateLocations = event => {
     event.preventDefault();
 
-    setLocations([
-      ...locations,
-      [city, country]
-    ])
+    if(city === '' || country === '') {
+      setError('Please fill in all the fields.')
+    } else {
+      setError('');
 
-    setCity('');
-    setCountry('');
+      setLocations([
+        ...locations,
+        [city, country]
+      ])
+
+      setCity('');
+      setCountry('');
+    }
   }
 
   return (
@@ -23,7 +30,8 @@ const Locations = () => {
       <form onSubmit={updateLocations}>
         <input type='text' name='city' id='city' placeholder='city name' value={city} onChange={e => setCity(e.target.value)} /><br/>
         <input type='text' name='country' id='country' placeholder='country name' value={country} onChange={e => setCountry(e.target.value)} /><br/>
-        <input type='submit' value='update locations' />
+        <input type='submit' value='update locations' /><br />
+        <span id='locationsError' className='error'>{error}</span>
       </form>
       <br/>
       Most Recent Location: {locations.length === 0 ? 'no locations recorded' : `${locations.slice(-1)[0][0]}, ${locations.slice(-1)[0][1]}` }<br />
